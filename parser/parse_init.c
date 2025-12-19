ab136d5ba918849eb7202822808ee4616f4264a3
Caio <kkndcaio@yahoo.com.br>
2025-12-19 16:37:35 -0300

    mais avancos no lexer e a criacao da funcao historic para cuidar do historico

--- parser/parse_init.c
+++ parser/parse_init.c
@@ -6,7 +6,7 @@
 /*   By: cacesar- <cacesar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 12:01:06 by cacesar-          #+#    #+#             */
-/*   Updated: 2025/12/18 19:21:06 by cacesar-         ###   ########.fr       */
+/*   Updated: 2025/12/19 16:34:35 by cacesar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,15 +50,27 @@ void	quotes(t_info*i, unsigned int *c, t_list*p, unsigned int *b)
 	*b = *c;
 }
 
-void	edcase(t_info*i, unsigned int *c, t_list*p, unsigned int *b)
+int	edcase(t_info*i, unsigned int *c, t_list*p, unsigned int *b)
 {
 	i->c2 = i->l[*c];
 	if (p->content && *c - *b >= 1)
 		p->content = ft_strjoin(i->str, ft_substr(i->l, *b, *c - *b - 1), 1, 1);
+	else if (*c - *b >= 1)
+		p->content = ft_substr(i->l, *b, *c - *b - 1);
+	if (p->content)
+		return (1);
+	while (i->l[*c] == i->c2)
+		*c++;
+	p->content = ft_substr(i->l, *b, *c - *b - 1);
+	*b = *c;
+	return (1);
 }
 
 t_list	*s_split(t_info*i, unsigned int *c, unsigned int b, t_list *p)
 {
+	int	end;
+
+	end = 0;
 	while (i->l[*c] == ' ')
 		*c++;
 	b = *c;
@@ -70,13 +82,11 @@ t_list	*s_split(t_info*i, unsigned int *c, unsigned int b, t_list *p)
 		if (ft_strchr("\'\"$", i->l[*c++]))
 			quotes(i->l, c, p, &b);
 	if (ft_strchr("&|<>\n", i->l[*c]))
-		edcase(i, c, p, &b);
-	if (ft_strchr("&|<>\n", i->l[*c]))
-		return (p);
-	if (!p->content && !ft_strchr("&|<>\n", i->l[*c]))
+		end = edcase(i, c, p, &b);
+	if (!p->content && !ft_strchr("&|<>\n", i->l[*c]) && !end)
 		p->content = ft_substr(i->l, b, *c - b);
-	else if (!ft_strchr("&|<>\n", i->l[*c]))
-		p->content = ft_strjoin(p->content, ft_substr(i->l, b, *c - b), 1, 1);
+	else if (!ft_strchr("&|<>\n", i->l[*c]) && !end)
+		p->content = ft_strjoin(p->content, ft_substr(i->l, b, *c - b), 1, 1 );
 	p->next = s_split(i, c, 0, 0);
 	return (p);
 }
@@ -106,10 +116,26 @@ int ft_chr_num(char*str, int c , int result, t_info*i)
 	return (result);
 }
 
+void historic(char	*l, int c1, int b)
+{
+	while (l[c1])
+	{
+		if (l[c1] == '\n')
+		{
+			add_history(ft_substr(l, b, c1 - b - 1));
+			b = c1;
+			while (l[c1] == ' ')
+				c1++;
+			continue ;
+		}
+		c1++;
+	}
+}
+
 int	main(int argc, char**argv, char**envp)
 {
-	t_info	data;
 	int		c1;
+	t_info	data;
 
 	env_maker(&data, -1, -1, envp);
 	while (argv && envp)
@@ -119,6 +145,7 @@ int	main(int argc, char**argv, char**envp)
 		if (!data.l)
 			continue ;
 		c1 = ft_chr_num(data.l, -1, 1, &data);
+		historic(data.l, 0, 0);
 		data.tree = ft_calloc(data.c2 + 1, 8);
 		while (c1)
 		{
