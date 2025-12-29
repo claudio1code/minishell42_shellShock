e511c99c93a9c887413f2cbadf861f086f1ef929
Claudio Matheus <claudio1.code@gmail.com>
2025-12-29 14:46:02 -0300

    fix: corrige a ordem das structs

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: claudio <claudio@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 17:38:07 by clados-s          #+#    #+#             */
-/*   Updated: 2025/12/25 14:56:51 by claudio          ###   ########.fr       */
+/*   Updated: 2025/12/25 18:56:19 by claudio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,19 @@
 // 	T_HEREDOC
 // }	t_type;
 
+typedef struct s_token
+{
+	char	*cmd;
+	char	**rdc;
+	char	**param;
+}	t_token;
 
 typedef struct s_logic
 {
 	char			*operator;
-
 	struct t_logic	*left;
 	struct t_logic	*right;
+	struct t_token	*cmd;
 }	t_logic;
 
 typedef struct s_info
@@ -49,13 +55,6 @@ typedef struct s_info
 	t_logic			**tree;
 }	t_info;
 
-typedef struct s_token
-{
-	char	*cmd;
-	char	**rdc;
-	char	**param;
-}	t_token;
-
 
 
 /*----------------------------------------------*/
@@ -66,5 +65,7 @@ char	*get_cmd_path(char *cmd, char **envp);
 int		handle_redirections(char **redir);
 void	child_cleanup(char *path);
 void	exec_cmd(t_token *token, t_info *info);
+void	exec_tree(t_logic *node, t_info *info);
+
 
 #endif
