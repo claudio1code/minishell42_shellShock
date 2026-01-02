ce81aa4dd94935b32ded8e3d0c238639bd34a284
Claudio <claudio1.code@gmail.com>
2026-01-02 09:56:54 -0300

    removendo função _exit proibida

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: clados-s <clados-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 13:36:47 by clados-s          #+#    #+#             */
-/*   Updated: 2025/12/23 10:47:12 by clados-s         ###   ########.fr       */
+/*   Updated: 2026/01/02 09:56:23 by clados-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,21 @@ static void	child_process(t_token *token, t_info *info)
 	char	*path;
 
 	if (handle_redirections(token->rdc) == -1)
-		_exit(1);
+		exit(1);
 	if (!token->param || !token->param[0])
-		_exit(0);
+		exit(0);
 	path = get_cmd_path(token->param[0], info->env);
 	if (!path)
 	{
 		ft_putstr_fd("minishell: ", 2);
 		ft_putstr_fd(token->param[0], 2);
 		ft_putendl_fd(": command not found", 2);
-		_exit(127);
+		exit(127);
 	}
 	execve(path, token->param, info->env);
 	perror("execve");
 	child_cleanup(path);
-	_exit(1);
+	exit(1);
 }
 
 void	exec_cmd(t_token *token, t_info *info)
