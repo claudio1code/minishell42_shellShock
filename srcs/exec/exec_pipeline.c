7fdcb205d6dd8b11b6ceaf548e713ab81eec6e11
Claudio Matheus <claudio1.code@gmail.com>
2026-02-02 18:23:25 -0300

    refatoracao(exec): prepara heredocs antes da execução do pipeline

--- srcs/exec/exec_pipeline.c
+++ srcs/exec/exec_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: clados-s <clados-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/25 19:04:49 by claudio           #+#    #+#             */
-/*   Updated: 2026/02/02 13:52:26 by clados-s         ###   ########.fr       */
+/*   Updated: 2026/02/02 18:22:45 by clados-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,7 @@ static void	wait_children(t_info *info)
 		info->exit_code = WEXITSTATUS(info->exit_code);
 }
 
-/* mudança na lógica para lista linkada
-verifico se exite um pipe anterior, se sim, ele redireciona
-para o stdin e fecha o prev, se houver um próximo, ele fecha
-pra leitura e redireciona o fd de escrito para o stdout, chama
-o exec_cmd que vai tratar os redirects e executar o comando, tbm
-guarda o status de saída na variavel exit_code 
-*/
+/*preparo os heredocs antes de iniciar o pipeline*/
 static void	child_process(t_token *token, t_info *info, int fd[2], int prev_fd)
 {
 	if (prev_fd != -1)
@@ -56,29 +50,21 @@ static void	child_process(t_token *token, t_info *info, int fd[2], int prev_fd)
 	exit(info->exit_code);
 }
 
-/*executa o pipeline de comandos, verifica se há um próximo
-token e cria um pipe se necessário*/
-void	exec_pipeline(t_token *token, t_info *info)
+
+static void	pipeline_loop(t_token *token, t_info *info)
 {
 	int		fd[2];
 	int		prev_fd;
 	pid_t	pid;
 
 	prev_fd = -1;
-	if (error(token))
-		return ;
-	if (token && !token->next && !token->prev && is_parent_builtin(token))
-	{
-		exec_cmd(token, info);
-		return ;
-	}
 	while (token)
 	{
 		if (token->next && pipe(fd) == -1)
 			return (perror("pipe"));
 		pid = fork();
 		if (pid == -1)
-			return (perror ("fork"));
+			return (perror("fork"));
 		if (pid == 0)
 			child_process(token, info, fd, prev_fd);
 		if (token->next)
@@ -88,4 +74,20 @@ void	exec_pipeline(t_token *token, t_info *info)
 		token = token->next;
 	}
 	wait_children(info);
+}
+
+/**/
+void	exec_pipeline(t_token *token, t_info *info)
+{
+	if (prepare_heredocs(token) == -1)
+	{
+		info->exit_code = 130;
+		return ;
+	}
+	if (error(token))
+		return ;
+	if (token && !token->next && !token->prev && is_parent_builtin(token))
+		exec_cmd(token, info);
+	else
+		run_pipeline_loop(token, info);
 }
